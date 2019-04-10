d7b1729c97fa875099ca66441d36d46e553f4e2c
eipc16 <pprzemek.312@gmail.com>
2019-04-10 20:28:39 +0200

    added algorithm choice

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 from Models.FutoshikiModel import FutoshikiModel
+from Models.SkyscraperModel import SkyscraperModel
 from Algorithms.ForwardChecking import ForwardChecking
+from Algorithms.BackTracking import BackTracking
 
 import sys, os
 
 
-def main(dir='./Data/Research/test_futo_6_0.txt'):
-    model = FutoshikiModel(dir)
+def start(csp_model, algo='forward_checking'):
+    if algo == 'forward_checking':
+        print('Running forward checking...')
+        csp = ForwardChecking(csp_model)
+    else:
+        print('Running back tracking...')
+        csp = BackTracking(csp_model)
 
-    csp = ForwardChecking(model)
-    print(csp.get_board(False))
     csp.run()
-    print(csp.get_info())
-    print(model.validate())
-    print(model.validate_non_zero())
 
 
 if __name__ == "__main__":
-    fn = sys.argv[1]
-    if os.path.exists(fn):
-        main(fn)
+    if len(sys.argv) > 2:
+        method = "forward_checking" if sys.argv[1] == "fc" else "back_tracking"
+        fn = sys.argv[2]
     else:
-        main()
+        method = "forward_checking"
+        fn = sys.argv[1]
+
+    if os.path.exists(fn):
+        if "futo" in fn:
+            print('Starting futoshiki')
+            model = FutoshikiModel(fn)
+            start(model, algo=method)
+        elif "sky" in fn:
+            print('Starting Skyscraper')
+            model = SkyscraperModel(fn)
+            start(model, algo=method)
+        else:
+            print('Inncorrect file name!')
+    else:
+        print('You have to provide valid path to the file!')
